2780b3daee31d0cc130ed7319fb1ca176d6c241a
jamjavascript <javascript4766@gmail.com>
2025-03-23 14:52:09 -0700

    Sidebar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
 
-import { ThemeProvider } from "@/providers/ThemeProvider"
-import { Toaster } from "@/components/ui/sonner"
+import { ThemeProvider } from "@/providers/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 import Header from "@/components/Header";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
 
 export const metadata: Metadata = {
-  title: "Goat Notes"
+  title: "Goat Notes",
 };
 
 export default function RootLayout({
@@ -18,19 +20,22 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body>
         <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-            >
-              <div className="flex flex-col min-h-screen w-full">  
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <SidebarProvider>
+            <AppSidebar />
+            <div className="flex min-h-screen w-full flex-col">
               <Header />
-              
+
               <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
                 {children}
               </main>
             </div>
-            <Toaster />
+          </SidebarProvider>
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
